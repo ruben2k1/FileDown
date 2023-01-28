f57a21e60add4ed523b4f6af09598da99afcecd3
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-01-28 04:20:24 +0100

    Añadir actualización

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-import { engine } from 'express-handlebars';
-import express from 'express';
+const express = require('express');
 const app = express();
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dual2 } from './src/database.cjs';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { engine } = require('express-handlebars');
+const path = require('path');
 
+//Settings
 app.set('port', 3000 || process.env.PORT);
-app.use(express.json());
-app.engine('.hbs', engine({
-    defaultLayout: "main",
-    extname: '.hbs',
-    partialsDir: __dirname + '/src/views/partials',
-    helpers : {}
-}));
 
-app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/src/views'));
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        p: "Hola",
-        title: "Plantilla"
-    });
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    partialsDir: __dirname + '/src/views/partials',
+    layoutsDir: __dirname + '/src/views/layouts',
+    helpers : require('./src/lib/hbs')
+}));
+
+app.set('view engine', '.hbs');
+
+app.use(express.json());
+
+//Global variables
+app.use((req, res, next) => {
+    next();
 })
+
+//Routes
+app.use(require('./src/routes'))
 
 app.use((req, res, next) => {
     res.status(404).send('Ruta no encontrada')
