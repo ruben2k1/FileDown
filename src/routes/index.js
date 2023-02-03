6f4a940b51457b0131eb28af30f3378a6bb7e196
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-02-03 02:36:43 +0100

    Añadir actualización

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,15 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     const [results, fields] = await pool.execute('SELECT * FROM CONTENIDO;');
-    res.render('index', {nombre: results[0][2]})
+    res.render('index', {nombre: results[0][2]});
+})
+
+router.post('/', async (req, res) => {
+    res.render('index', {nombre: req.body.search}); //Renderizar el search en otra plantilla con otra ruta, por ejemplo /search
+})
+
+router.get('/search', async (req, res) => {
+    res.render('index');
 })
 
 module.exports = router;
