d1f411309646f33a6987d69d56a77b293de37eec
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-05-03 10:30:06 +0200

    Añadir actualización

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: '',
-  database: 'filedown'
+  database: 'filedown',
+  rowsAsArray: false
 })
 
 module.exports = pool;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,27 +2,21 @@ const express = require('express');
 const pool = require('../database');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    pool.query('SELECT NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE LIMIT 20', (error, results, fields) => {
-        if (!error) {
-            res.render('index', {results, layout: 'main'});
-        }else{
-            console.log(error);
-        }
-    })
+router.get('/', async (req, res) => {
+    const [ results ] = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE LIMIT 20');
+    res.render('index', {layout: 'main', results});
 })
 
 router.post('/buscar', (req, res) => {
     res.redirect(`/buscar/${req.body.archivo}`);
 })
 
-router.get('/buscar/:archivo?', (req, res) => {
+router.get('/buscar/:archivo?', async (req, res) => {
     if (!req.params.archivo) {
         res.redirect('/');
     }else{
-        pool.query(`SELECT NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`, (error, results, fields) => {
-            res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
-        })
+        const [ results ] = await pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
+        res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
     }
 })
 
@@ -30,9 +24,8 @@ router.post('/buscar/:archivo?', (req, res) => {
     if (!req.params.archivo) {
         res.redirect('/');
     }else if (req.params.archivo==req.body.archivo){
-        pool.query(`SELECT NOMBRE, DESCRIPCION, RUTA_URL FROM CONTENIDO WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`, (error, results, fields) => {
-            res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
-        })
+        const [ results ] = pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL FROM CONTENIDO WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
+        res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
     }else{
         res.redirect(`/buscar/${req.body.archivo}`);
     }
@@ -42,14 +35,9 @@ router.get('/buscar', (req, res) => {
     res.redirect('/');
 })
 
-router.get('/novedades', (req, res) => {
-    pool.query('SELECT NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE LIMIT 20', (error, results, fields) => {
-        if (!error) {
-            res.render('index', {results, layout: 'novedades'});
-        }else{
-            console.log(error);
-        }
-    })
+router.get('/novedades', async (req, res) => {
+    const [ results ] = await pool.query('SELECT NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE LIMIT 20');
+    res.render('index', {results, layout: 'novedades'});
 })
 
 router.get('/contacto', (req, res) => {
@@ -60,16 +48,19 @@ router.post('/contacto', (req, res) => {
     res.redirect(`/buscar/${req.body.archivo}`);
 })
 
-router.post('/enviar', (req, res) => {
-    pool.query(`INSERT INTO CONTACTO (NOMBRE, CORREO, MENSAJE) VALUES ("${req.body.nombre}","${req.body.correo}","${req.body.mensaje}")`, (error, results, fields) => {
+router.post('/enviar', async (req, res) => {
+    const [ results ] = await pool.query(`INSERT INTO CONTACTO (NOMBRE, CORREO, MENSAJE) VALUES ("${req.body.nombre}","${req.body.correo}","${req.body.mensaje}")`);
+
+    if (results.affectedRows===1) {
         res.redirect('/');
-    })
+    }else{
+        res.redirect('/');
+    }
 })
 
-router.get('/archivo/:id', (req, res) => {
-    pool.query(`SELECT NOMBRE, DESCRIPCION, RUTA_URL FROM CONTENIDO WHERE ID=${req.params.id}`, (error, results, fields) => {
-        res.render('index', {busqueda: req.params.id, results, layout: 'buscar'});
-    })
+router.get('/archivo/:id', async (req, res) => {
+    const [ results ] = await pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL FROM ARCHIVOS_TABLE WHERE ID=${req.params.id}`);
+    res.render('index', {busqueda: req.params.id, results, layout: 'buscar'});
 })
 
 module.exports = router;
