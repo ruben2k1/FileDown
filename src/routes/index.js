9168ba8423e04f3c2d26f89a431ad7502d8a92af
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-05-05 01:44:41 +0200

    Añadir actualización

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,8 @@ router.get('/', async (req, res) => {
     const jpg = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_JPG');
     const rar = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_RAR');
     const pdf = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_PDF');
-    res.render('index', {layout: 'main', jpg: jpg[0], rar: rar[0], pdf: pdf[0]});
+    const xls = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_XLS');
+    res.render('index', {layout: 'main', jpg: jpg[0], rar: rar[0], pdf: pdf[0], xls: xls[0]});
 })
 
 router.post('/buscar', (req, res) => {
@@ -17,8 +18,8 @@ router.get('/buscar/:archivo?', async (req, res) => {
     if (!req.params.archivo) {
         res.redirect('/');
     }else{
-        const [ results ] = await pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
-        res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
+        const results = await pool.query(`SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_TABLE WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
+        res.render('index', {busqueda: req.params.archivo, layout: 'buscar', results: results[0]});
     }
 })
 
@@ -26,8 +27,8 @@ router.post('/buscar/:archivo?', (req, res) => {
     if (!req.params.archivo) {
         res.redirect('/');
     }else if (req.params.archivo==req.body.archivo){
-        const [ results ] = pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL FROM CONTENIDO WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
-        res.render('index', {busqueda: req.params.archivo, results, layout: 'buscar'});
+        const results = pool.query(`SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL FROM CONTENIDO WHERE LOWER(NOMBRE) LIKE LOWER('%${req.params.archivo}%')`);
+        res.render('index', {busqueda: req.params.archivo, layout: 'buscar', results: results[0]});
     }else{
         res.redirect(`/buscar/${req.body.archivo}`);
     }
@@ -38,8 +39,8 @@ router.get('/buscar', (req, res) => {
 })
 
 router.get('/novedades', async (req, res) => {
-    const [ results ] = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, RUTA_URL, CONCAT(RUTA_IMG, FORMATO) AS RUTA FROM ARCHIVOS_TABLE LIMIT 20');
-    res.render('index', {layout: 'novedades', results});
+    const results = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_TABLE');
+    res.render('index', {layout: 'novedades', results: results[0]});
 })
 
 router.get('/contacto', (req, res) => {
@@ -76,15 +77,16 @@ router.get('/archivo/:tipo/:id', async (req, res) => {
             results = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_PDF')
             res.render('index', {busqueda: req.params.id, layout: 'archivo', results: results[0]});
             break;
-        case 'pdf':
+        case 'xls':
             results = await pool.query('SELECT ID, NOMBRE, DESCRIPCION, CONCAT(RUTA_IMG, FORMATO_IMG) AS RUTA_IMG, FECHA, RUTA_URL FROM ARCHIVOS_XLS')
             res.render('index', {busqueda: req.params.id, layout: 'archivo', results: results[0]});
             break;
         default:
+            if (!req.params.id) {
+                res.redirect('/');
+            }
             break;
     }
-
-    
 })
 
 module.exports = router;
